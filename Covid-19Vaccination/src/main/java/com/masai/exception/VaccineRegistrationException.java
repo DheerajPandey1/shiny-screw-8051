4c912d821ae45f9fbf412d99853adad764b1bfc0
DheerajPandey1 <dheerajpandey071@gmail.com>
2022-12-18 21:08:11 +0530

    finalday

---
+++ Covid-19Vaccination/src/main/java/com/masai/exception/VaccineRegistrationException.java
@@ -0,0 +1,11 @@
+package com.masai.exception;
+
+public class VaccineRegistrationException extends Exception{
+
+	public VaccineRegistrationException(String m) {
+		super(m);
+	}
+	public VaccineRegistrationException() {
+		
+	}
+}
--- Covid-19Vaccination/src/main/java/com/masai/repo/VaccineRegistrationDao.java
+++ Covid-19Vaccination/src/main/java/com/masai/repo/VaccineRegistrationDao.java
@@ -1,4 +1,9 @@
 package com.masai.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.masai.model.VaccineRegistration;
+
 public interface VaccineRegistrationDao extends JpaRepository<VaccineRegistration, Integer> {
 		
 		VaccineRegistration findByMobileNumber(String mobileNumber);
